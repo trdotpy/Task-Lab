bdd4a7d5bb6b3bff9505f17fdba54f51b975ff6f
trdotpy <102767855+trdotpy@users.noreply.github.com>
2023-03-21 17:28:18 -0400

    feature: add demo login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,29 @@
 import {
+  IconCategory2,
+  IconCircleDashed,
   IconPlayerPlayFilled,
   IconUser,
   IconUserCircle,
+  IconX,
 } from "@tabler/icons-react";
 import Link from "next/link";
 import React from "react";
 
 export default function Header() {
-  const DEMO_EMAIL = process.env.DEMO_EMAIL;
-  const DEMO_PASSWORD = process.env.DEMO_PASSWORD;
-
-  const handleDemoLogin = (event) => {
-    event.preventDefault();
-    const auth0LoginUrl = `${process.env.NEXT_PUBLIC_AUTH0_DOMAIN}/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}&scope=openid%20profile%20email&connection=Username-Password-Authentication&email=${DEMO_EMAIL}&password=${DEMO_PASSWORD}`;
-    window.location.href = auth0LoginUrl;
-  };
   return (
     <div>
-      <header className="z-50 flex w-full flex-wrap border-b border-gray-200 bg-white py-3 text-sm dark:border-gray-700 dark:bg-gray-800 sm:flex-nowrap sm:justify-start sm:py-0">
+      <header className="z-50 flex w-full flex-wrap border-b border-gray-700 bg-[#2A324B] py-3 text-sm sm:flex-nowrap sm:justify-start">
         <nav
           className="relative mx-auto w-full max-w-7xl px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
           aria-label="Global"
         >
           <div className="flex items-center justify-between">
-            <a
-              className="flex-none text-xl font-semibold dark:text-white"
-              href="#"
-              aria-label="Brand"
-            >
-              Task Lab
-            </a>
+            <Link className="flex items-center" href="/">
+              <IconCircleDashed className="h-8 w-auto text-[#FF6663]" />
+              <h1 className="flex-none rounded px-3 py-1 text-xl font-semibold tracking-tight text-white hover:bg-gray-600">
+                TaskLab
+              </h1>
+            </Link>
             <div className="sm:hidden">
               <button
                 type="button"
@@ -38,27 +32,8 @@ export default function Header() {
                 aria-controls="navbar-collapse-with-animation"
                 aria-label="Toggle navigation"
               >
-                <svg
-                  className="h-4 w-4 hs-collapse-open:hidden"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                  />
-                </svg>
-                <svg
-                  className="hidden h-4 w-4 hs-collapse-open:block"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                </svg>
+                <IconCategory2 className="h-4 w-4 hs-collapse-open:hidden" />
+                <IconX className="hidden h-4 w-4 hs-collapse-open:block" />
               </button>
             </div>
           </div>
@@ -67,36 +42,24 @@ export default function Header() {
             className="hs-collapse hidden grow basis-full overflow-hidden transition-all duration-300 sm:block"
           >
             <div className="mt-5 flex flex-col gap-y-4 gap-x-0 sm:mt-0 sm:flex-row sm:items-center sm:gap-y-0 sm:gap-x-7 sm:pl-7">
-              <a
-                className="font-medium text-blue-600 dark:text-blue-500 sm:py-6"
-                href="#"
-                aria-current="page"
-              >
-                Landing
-              </a>
-              <a
-                className="font-medium text-gray-500 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 sm:py-6"
-                href="#"
-              >
-                Account
-              </a>
-
               <div className="flex items-center gap-x-2 sm:ml-auto">
                 <Link
-                  className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-500"
+                  className="flex items-center gap-x-2 font-medium"
                   href="/api/auth/login"
                 >
-                  <IconUser className="h-4 w-4" />
-                  Log in
+                  <button className="flex items-center gap-x-2 rounded px-2 py-1 font-medium text-gray-300 hover:bg-gray-600">
+                    Log in
+                  </button>
                 </Link>
 
-                <button
-                  className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-500"
-                  onClick={handleDemoLogin}
+                <Link
+                  className="flex items-center gap-x-2 font-medium text-gray-500"
+                  href="/trial"
                 >
-                  <IconPlayerPlayFilled className="h-4 w-4" />
-                  Free Demo
-                </button>
+                  <button className="flex items-center gap-x-2 rounded px-2 py-1 font-medium text-gray-300 hover:bg-gray-600">
+                    Trial
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
