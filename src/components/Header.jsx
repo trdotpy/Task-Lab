7c9a8178391442d5f22f938d0072e1f7d3b3823b
trdotpy <102767855+trdotpy@users.noreply.github.com>
2023-10-24 14:39:54 -0400

    Update homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,68 +4,60 @@ import Link from "next/link";
 import React from "react";
 
 export default function Header() {
-  return (
-    <>
-      <header className="z-50 flex w-full flex-wrap border-b bg-transparent py-3 text-sm sm:flex-nowrap sm:justify-start">
-        <nav
-          className="relative mx-auto w-full max-w-6xl px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex items-center justify-between">
-            <Link className="flex items-center rounded px-3 py-1" href="/">
-              <Image
-                src="/assets/tasklab-logo-blk.png"
-                alt="TaskLab"
-                height={35}
-                width={35}
-              />
-            </Link>
-            <div className="sm:hidden">
-              <button
-                type="button"
-                className="hs-collapse-toggle inline-flex items-center justify-center gap-2 rounded border border-jet-100 p-2 align-middle text-sm font-medium text-jet-200 shadow-sm transition-all"
-                data-hs-collapse="#navbar-collapse-with-animation"
-                aria-controls="navbar-collapse-with-animation"
-                aria-label="Toggle navigation"
-              >
-                <IconMenu2 className="h-4 w-4 hs-collapse-open:hidden" />
-                <IconMinus className="hidden h-4 w-4 hs-collapse-open:block" />
-              </button>
-            </div>
-          </div>
-          <div
-            id="navbar-collapse-with-animation"
-            className="hs-collapse hidden grow basis-full overflow-hidden transition-all duration-300 sm:block"
-          >
-            <div className="mt-5 flex flex-col gap-y-4 gap-x-0 sm:mt-0 sm:flex-row sm:items-center sm:gap-y-0 sm:gap-x-7 sm:pl-7">
-              <div className="flex flex-col justify-center sm:flex-row">
-                {/* <button className="rounded px-3 py-1.5 text-base text-jet-800 hover:bg-gray-100">
-                  Home
-                </button>
-                <button className="rounded px-3 py-1.5 text-base text-jet-800 hover:bg-gray-100">
-                  Pricing
-                </button>
-                <button className="rounded px-3 py-1.5 text-base text-jet-800 hover:bg-gray-100">
-                  Contact
-                </button> */}
-              </div>
-              <div className="flex items-center justify-center gap-x-2 sm:ml-auto">
-                <Link href="/api/auth/login">
-                  <button className="inline-flex items-center justify-center gap-2 rounded border-[1px] border-jet-100 py-2 px-4 text-sm text-jet-600 transition-all hover:border-jet-400">
-                    Account
-                  </button>
-                </Link>
-
-                <Link className="flex items-center gap-x-2" href="/trial">
-                  <button className="inline-flex items-center justify-center gap-2 rounded border-jet-100 bg-bitter-500 py-2 px-4 text-sm text-white transition-all hover:border-jet-400 hover:bg-bitter-600">
-                    Free Trial
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <header className="z-50 flex w-full flex-wrap border-b bg-transparent py-3 text-sm sm:flex-nowrap sm:justify-start">
+                <nav
+                    className="relative mx-auto w-full max-w-6xl px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+                    aria-label="Global"
+                >
+                    <div className="flex items-center justify-between">
+                        <Link
+                            className="flex items-center rounded px-3 py-1"
+                            href="/"
+                        >
+                            <Image
+                                src="/assets/tasklab-logo-blk.png"
+                                alt="TaskLab"
+                                height={35}
+                                width={35}
+                            />
+                        </Link>
+                        <div className="sm:hidden">
+                            <button
+                                type="button"
+                                className="hs-collapse-toggle inline-flex items-center justify-center gap-2 rounded border border-jet-100 p-2 align-middle text-sm font-medium text-jet-200 shadow-sm transition-all"
+                                data-hs-collapse="#navbar-collapse-with-animation"
+                                aria-controls="navbar-collapse-with-animation"
+                                aria-label="Toggle navigation"
+                            >
+                                <IconMenu2 className="h-4 w-4 hs-collapse-open:hidden" />
+                                <IconMinus className="hidden h-4 w-4 hs-collapse-open:block" />
+                            </button>
+                        </div>
+                    </div>
+                    <div
+                        id="navbar-collapse-with-animation"
+                        className="hs-collapse hidden grow basis-full overflow-hidden transition-all duration-300 sm:block"
+                    >
+                        <div className="mt-5 flex flex-col gap-y-4 gap-x-0 sm:mt-0 sm:flex-row sm:items-center sm:gap-y-0 sm:gap-x-7 sm:pl-7">
+                            <div className="flex flex-col justify-center sm:flex-row"></div>
+                            <div className="flex items-center justify-center gap-x-2 sm:ml-auto">
+                                <Link href="/api/auth/login">
+                                    <button className="inline-flex items-center justify-center gap-2 rounded border-[1px] border-jet-100 py-2 px-4 text-sm text-jet-600 transition-all hover:border-jet-400">
+                                        Sign in
+                                    </button>
+                                </Link>
+                                <Link href="/api/auth/login">
+                                    <button className="inline-flex items-center justify-center gap-2 rounded border-[1px] border-jet-100 bg-jet-800 py-2 px-4 text-sm text-white transition-all hover:border-jet-400">
+                                        Sign up
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </>
+    );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,49 +5,49 @@ import React from "react";
 import Brands from "./Brands";
 
 export default function Hero() {
-  return (
-    <>
-      <div className="mx-auto mt-16 max-w-6xl px-4 sm:px-6 lg:px-8">
-        <div className="grid gap-4 md:grid-cols-2 md:items-center md:gap-8 xl:gap-20">
-          <div>
-            <h1 className="block text-center text-4xl tracking-tight text-jet-800 sm:text-left sm:text-5xl lg:text-6xl lg:leading-tight">
-              Revolutionize your organization&apos;s workflow with{" "}
-              <span className="text-bitter-500">TaskLab</span>
-            </h1>
-            <p className="mt-6 text-center text-base text-gray-500 sm:text-left sm:text-lg">
-              Ready to see what TaskLab can do for your team? Sign up for a free
-              trial today and experience the power of effortless project
-              management.
-            </p>
+    return (
+        <>
+            <div className="mx-auto mt-16 max-w-6xl px-4 sm:px-6 lg:px-8">
+                <div className="grid gap-4 md:grid-cols-2 md:items-center md:gap-8 xl:gap-20">
+                    <div>
+                        <h1 className="block text-center text-4xl tracking-tight text-jet-800 sm:text-left sm:text-5xl lg:text-6xl lg:leading-tight">
+                            Revolutionize your organization&apos;s workflow with{" "}
+                            <span className="text-bitter-500">TaskLab</span>
+                        </h1>
+                        <p className="mt-6 text-center text-base text-gray-500 sm:text-left sm:text-lg">
+                            Ready to see what TaskLab can do for your team? Sign
+                            up for a free trial today and experience the power
+                            of effortless project management.
+                        </p>
 
-            <div className="mt-7 grid w-full justify-center gap-3 sm:inline-flex sm:justify-start">
-              <Link href="/trial">
-                <button className="inline-flex items-center justify-center gap-2 rounded border-[1px] border-jet-100 bg-jet-800 py-3 px-4 text-sm text-white transition-all hover:border-jet-400">
-                  Get Started
-                  <IconChevronRight className="h-5 w-5" />
-                </button>
-              </Link>
-              <Link href="https://github.com/trdotpy/Task-Lab">
-                <button className="inline-flex items-center justify-center gap-2 rounded border-[1px] border-jet-100 py-3 px-4 text-sm text-jet-600 transition-all hover:border-jet-400">
-                  Documentation
-                </button>
-              </Link>
+                        <div className="mt-7 grid w-full justify-center gap-3 sm:inline-flex sm:justify-start">
+                            <Link href="/api/auth/login">
+                                <button className="inline-flex items-center justify-center gap-2 rounded border-[1px] border-jet-100 bg-jet-800 py-3 px-4 text-sm text-white transition-all hover:border-jet-400">
+                                    Get Started
+                                    <IconChevronRight className="h-5 w-5" />
+                                </button>
+                            </Link>
+                            <Link href="https://github.com/trdotpy/Task-Lab">
+                                <button className="inline-flex items-center justify-center gap-2 rounded border-[1px] border-jet-100 py-3 px-4 text-sm text-jet-600 transition-all hover:border-jet-400">
+                                    Documentation
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className="relative ml-4">
+                        <Image
+                            className="w-full rounded-md"
+                            src="https://res.cloudinary.com/drij60fru/image/upload/v1679461566/pexels-photo-3183150_fkygjy.jpg"
+                            alt="hero image"
+                            width={1200}
+                            height={900}
+                        />
+                        {/* <div className="absolute inset-0 -z-[1] mt-4 -mb-4 mr-4 -ml-4 h-full w-full rounded-md bg-gradient-to-tr from-bice-600 via-white/0 to-white/0 lg:mt-6 lg:-mb-6 lg:mr-6 lg:-ml-6"></div> */}
+                    </div>
+                </div>
+                <Brands />
             </div>
-          </div>
-
-          <div className="relative ml-4">
-            <Image
-              className="w-full rounded-md"
-              src="https://res.cloudinary.com/drij60fru/image/upload/v1679461566/pexels-photo-3183150_fkygjy.jpg"
-              alt="hero image"
-              width={1200}
-              height={900}
-            />
-            {/* <div className="absolute inset-0 -z-[1] mt-4 -mb-4 mr-4 -ml-4 h-full w-full rounded-md bg-gradient-to-tr from-bice-600 via-white/0 to-white/0 lg:mt-6 lg:-mb-6 lg:mr-6 lg:-ml-6"></div> */}
-          </div>
-        </div>
-					<Brands />
-      </div>
-    </>
-  );
+        </>
+    );
 }
